a23b0ac5d42f1074d5d1a8fded99183767fb4b1c
florianm <florian.mayer@dpaw.wa.gov.au>
2019-05-28 15:50:54 +0800

    #3 add tests

--- tests/testthat/test-get_attachment.R
+++ tests/testthat/test-get_attachment.R
@@ -15,3 +15,14 @@ test_that("get_attachment works", {
   expect_gte(nrow(fresh_parsed), 2) # submissions at the time of writing
   expect_true(file.exists(fresh_parsed$quadrat_photo[[1]]))
 })
+
+test_that("attachment_url works", {
+  data_url <- "https://sandbox.central.opendatakit.org/v1/projects/14/forms/build_Flora-Quadrat-0-2_1558575936.svc"
+  uuid <- "uuid:c0f9ce58-4388-4e7b-98d7-feac459d2e12"
+  fn <- "1558579592153.jpg"
+
+  expected_url <- "https://sandbox.central.opendatakit.org/v1/projects/14/forms/build_Flora-Quadrat-0-2_1558575936/submissions/uuid:c0f9ce58-4388-4e7b-98d7-feac459d2e12/attachments/1558579592153.jpg"
+  calculated_url <- attachment_url(data_url, uuid, fn)
+
+  expect_equal(calculated_url, expected_url)
+})
