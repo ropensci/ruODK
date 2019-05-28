ba47617679a80e6c9048f33b2aa4a9ef7110f5c2
florianm <florian.mayer@dpaw.wa.gov.au>
2019-05-28 14:00:06 +0800

    #3 test get_attachment with verbose TRUE

--- tests/testthat/test-get_attachment.R
+++ tests/testthat/test-get_attachment.R
@@ -5,7 +5,9 @@ test_that("get_attachment works", {
   fresh_raw <- get_submissions(data_url)
   fresh_parsed <- fresh_raw %>% parse_submissions() %>%
     dplyr::rename(uuid=`.__id`) %>%
-    dplyr::mutate(quadrat_photo = get_attachment(data_url, uuid, quadrat_photo, local_dir = tempdir()))
+    dplyr::mutate(
+      quadrat_photo = get_attachment(
+        data_url, uuid, quadrat_photo, local_dir = tempdir(), verbose=TRUE))
   expect_gte(nrow(fresh_parsed), 2) # submissions at the time of writing
   expect_true(file.exists(fresh_parsed$quadrat_photo[[1]]))
 })
