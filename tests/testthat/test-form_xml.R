9f5753dccf9d7dd399a71bf12b09ea34e4217fde
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2019-08-01 18:37:21 +0800

    codecov: you missed a spot. me: eeeeh

--- tests/testthat/test-form_xml.R
+++ tests/testthat/test-form_xml.R
@@ -25,10 +25,10 @@ test_that("form_xml returns an xml_document with parse=FALSE", {
   fxml <- form_xml(
     Sys.getenv("ODKC_TEST_PID"),
     Sys.getenv("ODKC_TEST_FID"),
-    parse = TRUE,
+    parse = FALSE,
     url = Sys.getenv("ODKC_TEST_URL"),
     un = Sys.getenv("ODKC_TEST_UN"),
     pw = Sys.getenv("ODKC_TEST_PW")
   )
-  testthat::expect_equal(class(fxml), "list")
+  testthat::expect_equal(class(fxml), c("xml_document", "xml_node"))
 })
