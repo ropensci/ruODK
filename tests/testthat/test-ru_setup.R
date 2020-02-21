903c3235696b6cf73f2526692e979b732c72c328
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-02-21 11:39:04 +0800

    test ru_setup: debug windows tests for tz

--- tests/testthat/test-ru_setup.R
+++ tests/testthat/test-ru_setup.R
@@ -129,8 +129,19 @@ test_that("ru_setup resets settings if given empty string", {
 })
 
 test_that("get_default_tz warns if tz explicitly set to empty string", {
+
+  tz <- get_default_tz()
+
+  # Override tz
   ru_setup(tz = "")
+
+  # Appveyor seems to be UTC
+  message("Default time zone after setting to empty string: ")
+  message(get_default_tz())
+
   testthat::expect_warning(get_default_tz())
+
+  ru_setup(tz = tz) # restore previous settings
 })
 
 test_that("ru_setup sets pid, fid, url if given service url", {
