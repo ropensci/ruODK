cdb53483f8b6f6eecb5b40f67e24cc25b6895b01
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-07-21 17:09:20 +0800

    Update tic

--- tic.R
+++ tic.R
@@ -1,14 +1,5 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks(
-  error_on="error",
-  args = c(
-    # "--no-manual",
-    "--as-cran",
-    # "--no-vignettes",
-    # "--no-build-vignettes",
-    "--no-multiarch"
-  )
-)
+do_package_checks()
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
