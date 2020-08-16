e1bb28634ed3aed2a339eb49450e64c9544a4316
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-08-16 18:25:54 +0800

    Update logo to fit ODK branding guidelines
    * Remove the official ODK logo
    * Update green turtle hatchling photo

--- data-raw/make_logo.R
+++ data-raw/make_logo.R
@@ -5,19 +5,20 @@
 library(hexSticker)
 library(showtext)
 sysfonts::font_add_google("Knewave", "knewave")
-# odklogo <- here::here("man", "figures", "odk.png")
+# sysfonts::font_add_google("Prosto One", "prosto")
+# sysfonts::font_add_google("Concert One", "concert")
 turtle <- here::here("man", "figures", "turtle.png")
 ruodklogo <- here::here("man", "figures", "ruODK.png")
 darkred <- "#a50b0b"
 # logo s_, text p_, bg h_
 hexSticker::sticker(
-  # odklogo,
   turtle,
-  asp = 0.684,
-  s_x = 1.0, s_y = 1.05, s_width = 1.4, # s_height = 0.1,
-  package = "ru", p_x = 0.5, p_y = 1, p_size = 50,
+  # asp = 0.684, dpi = 300,
+  s_x = 1.0, s_y = 1.00, s_width = 1.1, # s_height = 0.1,
+  package = "ruODK", p_x = 1, p_y = 0.43, p_size = 24,
   p_family = "knewave", p_color = darkred,
   h_fill = "#aaaaaa", h_color = darkred,
+  # url = "docs.ropensci.org/ruODK", u_size = 4, u_color = "#ffffff",
   white_around_sticker = T,
   filename = ruodklogo
 )
