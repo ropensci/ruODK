88af70e3d437774c574aa4d0bf68f01d4d42e859
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-08-18 08:00:26 +0800

    GHA: fix tic.R typo

--- tic.R
+++ tic.R
@@ -12,7 +12,7 @@ do_package_checks(
 
 get_stage("install") %>%
   # add_step(step_install_github(c("tidyverse/readr"))) %>%
-  add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
+  add_step(step_install_github("r-spatial/mapview", dependencies = TRUE))
   # add_step(step_install_cran("mapview"))
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
