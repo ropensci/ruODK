4a7a06173311ede427e23ae6ec9d3d9d6d569aa4
mtyszler <tyszler.jobs@gmail.com>
2020-10-30 09:40:03 +0100

    Included additional tests for the form schema extended

--- tests/testthat/test-form_schema_ext.R
+++ tests/testthat/test-form_schema_ext.R
@@ -10,4 +10,33 @@ test_that("form_schema_ext v8 returns a tibble with defaults", {
   testthat::expect_true(tibble::is_tibble(fsx))
 })
 
+test_that("form_schema_ext v8 in a form with no languages", {
+  fsx <- form_schema_ext(
+    svc ="https://sandbox.central.getodk.org/v1/projects/132/forms/sample_no_language.svc",
+    un = get_test_un(),
+    pw = get_test_pw(),
+    odkc_version = get_test_odkc_version()
+  )
+  testthat::expect_true(tibble::is_tibble(fsx))
+})
+
+test_that("form_schema_ext v8 in a form with label languages", {
+  fsx <- form_schema_ext(
+    svc = "https://sandbox.central.getodk.org/v1/projects/132/forms/sample_label_language.svc",
+    un = get_test_un(),
+    pw = get_test_pw(),
+    odkc_version = get_test_odkc_version()
+  )
+  testthat::expect_true(tibble::is_tibble(fsx))
+})
+
+test_that("form_schema_ext v8 in a form with label and choices languages", {
+  fsx <- form_schema_ext(
+    svc = "https://sandbox.central.getodk.org/v1/projects/132/forms/sample_label_and_choices_language.svc",
+    un = get_test_un(),
+    pw = get_test_pw(),
+    odkc_version = get_test_odkc_version()
+  )
+  testthat::expect_true(tibble::is_tibble(fsx))
+})
 # usethis::edit_file("R/form_schema_ext.R") # nolint
