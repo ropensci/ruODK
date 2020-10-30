ropensci/ruODK

commit ab48ee9b00fddaf5d294165e5301d4869cc9cfa5

Add refernce to form xml in api documentation

R/form_schema_ext.R
@@ -69,7 +69,8 @@
 #'   }
 # nolint start
 #' @seealso \url{https://odkcentral.docs.apiary.io/#reference/forms-and-submissions/'-individual-form/getting-form-schema-fields}
-#' @seealso \url{https://odkcentral.docs.apiary.io/#reference/forms-and-submissions/'-individual-form/retrieving-form-schema-json}
+#' @seealso \url{https://odkcentral.docs.apiary.io/#reference/forms-and-submissions/'-individual-form/retrieving-form-
+#' @seealso \url{https://odkcentral.docs.apiary.io/#reference/forms-and-submissions/'-individual-form/retrieving-form-xml}
 # nolint end
 #' @family restful-api
 #' @export

commit 6f3e753a63184035440d38e69589c69ddadabcbf

Reverse small error introduced by commit ab48ee9b00fddaf5d294165e5301d4869cc9cfa5

R/form_schema_ext.R
@@ -69,7 +69,7 @@
 #'   }
 # nolint start
 #' @seealso \url{https://odkcentral.docs.apiary.io/#reference/forms-and-submissions/'-individual-form/getting-form-schema-fields}
-#' @seealso \url{https://odkcentral.docs.apiary.io/#reference/forms-and-submissions/'-individual-form/retrieving-form-
+#' @seealso \url{https://odkcentral.docs.apiary.io/#reference/forms-and-submissions/'-individual-form/retrieving-form-schema-json}
 #' @seealso \url{https://odkcentral.docs.apiary.io/#reference/forms-and-submissions/'-individual-form/retrieving-form-xml}
 # nolint end
 #' @family restful-api