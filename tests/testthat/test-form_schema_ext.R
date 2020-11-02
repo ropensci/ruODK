4195613ac94fff729178b240b1e161e013772c3d
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-11-02 16:32:54 +0800

    Add a few tests for form_schema_ext

--- tests/testthat/test-form_schema_ext.R
+++ tests/testthat/test-form_schema_ext.R
@@ -8,41 +8,55 @@ test_that("form_schema_ext v8 returns a tibble with defaults", {
     odkc_version = get_test_odkc_version()
   )
   testthat::expect_true(tibble::is_tibble(fsx))
+  testthat::expect_true("label" %in% names(fsx))
+  testthat::expect_true("choices" %in% names(fsx))
+  testthat::expect_true("label_english" %in% names(fsx))
+  testthat::expect_true("choices_english" %in% names(fsx))
 })
 
 test_that("form_schema_ext v8 in a form with no languages", {
   fsx <- form_schema_ext(
     pid = get_test_pid(),
-    fid = Sys.getenv("ODKC_TEST_FID_I8N0", unset="I8n_no_lang")
+    fid = Sys.getenv("ODKC_TEST_FID_I8N0", unset="I8n_no_lang"),
     url = get_test_url(),
     un = get_test_un(),
     pw = get_test_pw(),
     odkc_version = get_test_odkc_version()
   )
   testthat::expect_true(tibble::is_tibble(fsx))
+  testthat::expect_true("label" %in% names(fsx))
+  testthat::expect_true("choices" %in% names(fsx))
 })
 
 test_that("form_schema_ext v8 in a form with label languages", {
   fsx <- form_schema_ext(
     pid = get_test_pid(),
-    fid = Sys.getenv("ODKC_TEST_FID_I8N1", unset="I8n_label_lng")
+    fid = Sys.getenv("ODKC_TEST_FID_I8N1", unset="I8n_label_lng"),
     url = get_test_url(),
     un = get_test_un(),
     pw = get_test_pw(),
     odkc_version = get_test_odkc_version()
   )
   testthat::expect_true(tibble::is_tibble(fsx))
+  testthat::expect_true("label_english_(en)" %in% names(fsx))
+  testthat::expect_true("label_french_(fr)" %in% names(fsx))
 })
 
 test_that("form_schema_ext v8 in a form with label and choices languages", {
   fsx <- form_schema_ext(
     pid = get_test_pid(),
-    fid = Sys.getenv("ODKC_TEST_FID_I8N2", unset="I8n_label_choices")
+    fid = Sys.getenv("ODKC_TEST_FID_I8N2", unset="I8n_label_choices"),
     url = get_test_url(),
     un = get_test_un(),
     pw = get_test_pw(),
     odkc_version = get_test_odkc_version()
   )
   testthat::expect_true(tibble::is_tibble(fsx))
+  testthat::expect_true("label" %in% names(fsx))
+  testthat::expect_true("choices" %in% names(fsx))
+  testthat::expect_true("label_english_(en)" %in% names(fsx))
+  testthat::expect_true("label_french_(fr)" %in% names(fsx))
+  testthat::expect_true("choices_english_(en)" %in% names(fsx))
+  testthat::expect_true("choices_french_(fr)" %in% names(fsx))
 })
 # usethis::edit_file("R/form_schema_ext.R") # nolint
