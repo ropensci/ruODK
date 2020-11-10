3c664ca43ffd64a09184bbc2d1e5a62e0e52fbf0
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-11-10 14:49:04 +0800

    GHA please

--- tic.R
+++ tic.R
@@ -12,14 +12,14 @@ do_package_checks(
 
 get_stage("install") %>%
   # add_step(step_install_github(c("tidyverse/readr"))) %>%
-  # add_step(step_install_cran("proj4")) %>%
-  # add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
-  # add_step(step_install_github("r-spatial/leafem", dependencies = TRUE)) %>%
-  # add_step(step_install_cran("listviewer")) %>%
-  add_step(step_install_github("gagolews/stringi", dependencies = TRUE, force = TRUE)) %>%
-  add_step(step_install_github("r-lib/cpp11", dependencies = TRUE))
   # https://stackoverflow.com/q/61875754/2813717 - install proj4
-  # add_step(step_install_cran("mapview"))
+  add_step(step_install_cran("proj4")) %>%
+  add_step(step_install_github("r-lib/cpp11", dependencies = TRUE)) %>%
+  add_step(step_install_github("r-spatial/sf", dependencies = TRUE)) %>%
+  add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
+  add_step(step_install_github("r-spatial/leafem", dependencies = TRUE)) %>%
+  add_step(step_install_cran("listviewer")) %>%
+  add_step(step_install_github("gagolews/stringi", dependencies = TRUE, force = TRUE))
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
