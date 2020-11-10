380bc67895f5a5d5e4b72e61b6dc522d252a9ba9
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-11-10 14:06:03 +0800

    Tighten up DESC, GHA: add listviewer

--- tic.R
+++ tic.R
@@ -15,7 +15,8 @@ get_stage("install") %>%
   add_step(step_install_cran("proj4")) %>%
   add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
   add_step(step_install_github("r-spatial/leafem", dependencies = TRUE)) %>%
-  add_step(step_install_cran("stringi")) %>%
+  add_step(step_install_cran("listviewer")) %>%
+  add_step(step_install_github("gagolews/stringi", dependencies = TRUE)) %>%
   add_step(step_install_github("r-lib/cpp11", dependencies = TRUE))
   # https://stackoverflow.com/q/61875754/2813717 - install proj4
   # add_step(step_install_cran("mapview"))
