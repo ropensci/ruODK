c99f2e64695bd6a5eed14c9f0f4c23625dee7022
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-08-19 09:38:28 +0800

    Resolve warnings in test-project_list

--- tests/testthat/test-project_list.R
+++ tests/testthat/test-project_list.R
@@ -36,80 +36,72 @@ test_that("project_list works", {
 # We test expected of missing authentication here once for all ruODK functions.
 
 test_that("project_list fails on missing crendentials", {
-  vcr::use_cassette("test_project_list1", {
-    testthat::expect_error(p <- project_list(
-      url = NULL,
-      un = NULL,
-      pw = NULL
-    ))
-  })
+  skip_on_cran()
+  testthat::expect_error(p <- project_list(
+    url = NULL,
+    un = NULL,
+    pw = NULL
+  ))
 })
 
 test_that("project_list fails on missing URL", {
-  vcr::use_cassette("test_project_list2", {
-    testthat::expect_error(p <- project_list(
-      url = NULL,
-      un = get_test_un(),
-      pw = get_test_pw()
-    ))
-  })
+  skip_on_cran()
+  testthat::expect_error(p <- project_list(
+    url = NULL,
+    un = get_test_un(),
+    pw = get_test_pw()
+  ))
 })
 
 test_that("project_list fails on missing username", {
-  vcr::use_cassette("test_project_list3", {
-    testthat::expect_error(p <- project_list(
-      url = get_test_url(),
-      un = NULL,
-      pw = get_test_pw()
-    ))
-  })
+  skip_on_cran()
+  testthat::expect_error(p <- project_list(
+    url = get_test_url(),
+    un = NULL,
+    pw = get_test_pw()
+  ))
 })
 
 test_that("project_list fails on missing password", {
-  vcr::use_cassette("test_project_list4", {
-    testthat::expect_error(p <- project_list(
-      url = get_test_url(),
-      un = get_test_un(),
-      pw = NULL
-    ))
-  })
+  skip_on_cran()
+  testthat::expect_error(p <- project_list(
+    url = get_test_url(),
+    un = get_test_un(),
+    pw = NULL
+  ))
 })
 
 test_that("project_list aborts on missing credentials", {
-  vcr::use_cassette("test_project_list5", {
-    testthat::expect_error(p <- project_list(
-      url = "",
-      un = get_test_un(),
-      pw = get_test_pw()
-    ))
-  })
+  skip_on_cran()
+  testthat::expect_error(p <- project_list(
+    url = "",
+    un = get_test_un(),
+    pw = get_test_pw()
+  ))
 
-  vcr::use_cassette("test_project_list6", {
-    testthat::expect_error(p <- project_list(
-      url = get_test_url(),
-      un = "",
-      pw = get_test_pw()
-    ))
-  })
+  testthat::expect_error(p <- project_list(
+    url = get_test_url(),
+    un = "",
+    pw = get_test_pw()
+  ))
 
-  vcr::use_cassette("test_project_list7", {
-    testthat::expect_error(p <- project_list(
-      url = get_test_url(),
-      un = get_test_un(),
-      pw = ""
-    ))
-  })
+  testthat::expect_error(p <- project_list(
+    url = get_test_url(),
+    un = get_test_un(),
+    pw = ""
+  ))
 })
 
-test_that("project_list warns on wrong credentials", {
-  vcr::use_cassette("test_project_list8", {
-    testthat::expect_error(p <- project_list(
-      url = "wrong_url",
-      un = get_test_un(),
-      pw = get_test_pw()
-    ))
-  })
+test_that("project_list warns on wrong URL", {
+  testthat::expect_error(p <- project_list(
+    url = "wrong_url",
+    un = get_test_un(),
+    pw = get_test_pw()
+  ))
+})
 
+
+test_that("project_list warns on wrong credentials", {
   vcr::use_cassette("test_project_list9", {
     testthat::expect_error(p <- project_list(
       url = get_test_url(),
