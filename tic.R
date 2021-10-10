1709b50ab8c32fd2373dac53ecbf05be5c96dd82
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-10-10 08:17:33 +0800

    GHA: tic.R only installs pkgs for ubuntu-20.04

--- tic.R
+++ tic.R
@@ -10,18 +10,20 @@ do_package_checks(
   )
 )
 
-get_stage("install") %>%
-  # add_step(step_install_github(c("tidyverse/readr"))) %>%
-  # https://stackoverflow.com/q/61875754/2813717 - install proj4
-  add_step(step_install_cran("proj4")) %>%
-  # sf install fixed by cpp11
-  add_step(step_install_github("r-lib/cpp11", dependencies = TRUE)) %>%
-  add_step(step_install_github("r-spatial/sf", dependencies = TRUE)) %>%
-  add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
-  add_step(step_install_github("r-spatial/leafem", dependencies = TRUE)) %>%
-  add_step(step_install_cran("listviewer")) %>%
-  # libicui8n not found: fixed by stringi forced install
-  add_step(step_install_github("gagolews/stringi", dependencies = TRUE, force = TRUE))
+if(ci_get_env("matrix.config.os") == "ubuntu-20.04"){
+  get_stage("install") %>%
+    # add_step(step_install_github(c("tidyverse/readr"))) %>%
+    # https://stackoverflow.com/q/61875754/2813717 - install proj4
+    add_step(step_install_cran("proj4")) %>%
+    # sf install fixed by cpp11
+    add_step(step_install_github("r-lib/cpp11", dependencies = TRUE)) %>%
+    add_step(step_install_github("r-spatial/sf", dependencies = TRUE, force = TRUE)) %>%
+    add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
+    add_step(step_install_github("r-spatial/leafem", dependencies = TRUE)) %>%
+    # add_step(step_install_cran("listviewer")) %>%
+    # libicui8n not found: fixed by stringi forced install
+    add_step(step_install_github("gagolews/stringi", dependencies = TRUE, force = TRUE))
+}
 
 # # rOpenSci build their own docs, see build at
 # # https://dev.ropensci.org/job/ruODK/lastBuild/console
