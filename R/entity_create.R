3c4ca873b22bc3033b1029995a796b02e08f4d03
Florian Mayer <florian.mayer@dpc.wa.gov.au>
2024-07-24 12:15:27 +0800

    Complete entity_create
    * Add test for creating multiple entities
    * Update man with working example to create multiple entities

--- R/entity_create.R
+++ R/entity_create.R
@@ -54,14 +54,25 @@
 #' @template param-pid
 #' @template param-did
 #' @param label (character) The Entity label which must be a non-empty string.
+#'   If the label is given, a single entity is created using `data`, `notes`,
+#'   and `uuid` if given.
+#'   If the label is kept at the default (or omitted), multiple entities are
+#'   created using `data` and `notes` and ignoring `uuid`.
 #'   Default: `""`.
+#' @param uuid (character) A single UUID to assign to the entity.
+#'   Default: `""`. With the default, Central will create and assign a UUID.
+#'   This parameter is only used when creating a single entity (`label`
+#'   non-empty) and ignored when creating multiple entities (`label` empty).
 #' @param notes (character) Metadata about the request which can be retrieved
 #'   using the entity audit log.
 #'   Default: `""`.
 #' @param data (list) A named list of Entity properties to create a single
 #'    Entity, or a nested list with an array of Entity data to create multiple
-#'    Entities. See details for the exact format.
-#'   Default: `list()`.
+#'    Entities. The nested lists representing individual entities must be valid
+#'    as in they must contain a label, valid data for the respective entity
+#'    properties, and can contain an optional UUID.
+#'    See details and the ODK documentation for the exact format.
+#'    Default: `list()`.
 #' @template param-url
 #' @template param-auth
 #' @template param-retries
@@ -100,24 +111,39 @@
 #' )
 #' ec
 #'
-#' # Create multiple entities
-#' eb <- entity_create(
-#'   did = did,
-#'   notes = "Metadata about the created entities",
-#'   data = list(
-#'     "entities" = c(
-#'       list("label" = "Entity 1", "data" = list("field1" = "value1")),
-#'       list("label" = "Entity 2", "data" = list("field1" = "value2"))
-#'     ),
-#'     "source" = list("name" = "file.csv", "size" = 100)
+#' # Create multiple entities, example: test form "problems"
+#' label <- c(
+#'   glue::glue(
+#'     "Entity {nrow(en) + 1} created by ruODK package test on {Sys.time()}"
+#'   ),
+#'   glue::glue(
+#'     "Entity {nrow(en) + 2} created by ruODK package test on {Sys.time()}"
 #'   )
 #' )
-#' eb
+#' notes <- glue::glue("Two entities created by ruODK package test on {Sys.time()}")
+#' status <- c("needs_followup", "needs_followup")
+#' details <- c("ruODK package test", "ruODK package test")
+#' geometry <- c("-33.2 115.0 0.0 0.0", "-33.2 115.0 0.0 0.0")
+#' data <- data.frame(status, details, geometry, stringsAsFactors = FALSE)
+#' request_data <- list(
+#'   "entities" = data.frame(label, data = I(data), stringsAsFactors = FALSE),
+#'   "source" = list("name" = "file.csv", "size" = 100)
+#' )
+#'
+#' # Compare request_data to the schema expected by Central
+#' # https://docs.getodk.org/central-api-entity-management/#creating-entities
+#' # jsonlite::toJSON(request_data, pretty = TRUE, auto_unbox = TRUE)
+#'
+#' ec <- entity_create(
+#'   did = did,
+#'   notes = notes,
+#'   data = request_data
+#' )
 #' }
 entity_create <- function(pid = get_default_pid(),
                           did = "",
                           label = "",
-                          uuid = NULL,
+                          uuid = "",
                           notes = "",
                           data = list(),
                           url = get_default_url(),
@@ -155,7 +181,7 @@ entity_create <- function(pid = get_default_pid(),
   } else {
     ru_msg_info("Non-empty label: creating single entity.")
     # If uuid is not NULL, include in body
-    if (!is.null(uuid)) {
+    if (uuid != "") {
       body <- list("uuid" = uuid, "label" = label, "data" = data)
     } else {
       body <- list("label" = label, "data" = data)
--- tests/testthat/test-entity_create.R
+++ tests/testthat/test-entity_create.R
@@ -43,57 +43,55 @@ test_that("entity_create creates single entities", {
   testthat::expect_equal(names(ec), ec_names)
 
   time_created <- lubridate::ymd_hms(ec$created_at, tz = "UTC")
-  time_before_create <- lubridate::ymd_hms(
-    time_before_created,
-    tz = "Australia/Perth"
-  )
+  time_before_create <- lubridate::ymd_hms(time_before_created, tz = "Australia/Perth")
 
   testthat::expect_gte(time_created, time_before_create)
 })
 
 test_that("entity_create creates multiple entities", {
-  testthat::skip("TODO")
+  ru_setup(
+    pid = get_test_pid(),
+    url = get_test_url(),
+    un = get_test_un(),
+    pw = get_test_pw(),
+    odkc_version = get_test_odkc_version()
+  )
 
-  # ru_setup(
-  #   pid = get_test_pid(),
-  #   url = get_test_url(),
-  #   un = get_test_un(),
-  #   pw = get_test_pw(),
-  #   odkc_version = get_test_odkc_version()
-  # )
-  #
-  # el <- entitylist_list()
-  #
-  # # Entity List name (dataset ID, did)
-  # did <- el$name[1]
-  #
-  # # All Entities of Entity List
-  # en <- entity_list(did = did)
-  # # ed <- entity_detail(did=did, eid = en$uuid[1])
-  #
-  # time_before_created <- Sys.time()
-  #
-  # # Create a single entity
-  # lab = glue::glue(
-  #   "Entity {nrow(en) + 1} created by ruODK package test on {Sys.time()}"
-  # )
-  # ec <- entity_create(
-  #   did = did,
-  #   label = lab,
-  #   notes = lab,
-  #   data = list(
-  #     "status" = "needs_followup",
-  #     "details" = "ruODK package test",
-  #     "geometry"="-33.2 115.0 0.0 0.0"
-  #   )
-  # )
-  #
-  # testthat::expect_equal(names(ec), ec_names)
-  #
-  # time_created =  lubridate::ymd_hms(ec$created_at, tz = "UTC")
-  # time_before_create = lubridate::ymd_hms(time_before_created, tz = "Australia/Perth")
-  #
-  # testthat::expect_gte(time_created, time_before_create)
+  el <- entitylist_list()
+
+  # Entity List name (dataset ID, did)
+  did <- el$name[1]
+
+  # All Entities of Entity List
+  en <- entity_list(did = did)
+
+  label <- c(
+    glue::glue(
+      "Entity {nrow(en) + 1} created by ruODK package test on {Sys.time()}"
+    ),
+    glue::glue(
+      "Entity {nrow(en) + 2} created by ruODK package test on {Sys.time()}"
+    )
+  )
+  notes <- glue::glue("Two entities created by ruODK package test on {Sys.time()}")
+  status <- c("needs_followup", "needs_followup")
+  details <- c("ruODK package test", "ruODK package test")
+  geometry <- c("-33.2 115.0 0.0 0.0", "-33.2 115.0 0.0 0.0")
+  data <- data.frame(status, details, geometry, stringsAsFactors = FALSE)
+  request_data <- list(
+    "entities" = data.frame(label, data = I(data), stringsAsFactors = FALSE),
+    "source" = list("name" = "file.csv", "size" = 100)
+  )
+  # jsonlite::toJSON(request_data, pretty = TRUE, auto_unbox = TRUE)
+
+  ec <- entity_create(
+    did = did,
+    notes = notes,
+    data = request_data
+  )
+
+  testthat::expect_equal(names(ec), "success")
+  testthat::expect_true(ec$success)
 })
 
 test_that("entity_create input gatechecks", {
@@ -117,9 +115,7 @@ test_that("entity_create input gatechecks", {
   time_before_created <- Sys.time()
 
   # Create a single entity
-  lab <- glue::glue(
-    "Entity {nrow(en) + 1} created by ruODK package test on {Sys.time()}"
-  )
+  lab <- glue::glue("Entity {nrow(en) + 1} created by ruODK package test on {Sys.time()}")
 
   # ODKC version not supported
   testthat::expect_warning(entity_create(
